f32fc8d29ec56483ae9144f0d24216e97b01ab71
Allen <105831767+allenjbb@users.noreply.github.com>
2024-03-18 22:46:28 -0700

    Update Redirector.jsx
    Add debugging console log

--- src/components/Redirector.jsx
+++ src/components/Redirector.jsx
@@ -1,15 +1,17 @@
 import { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { useVisitedPages } from '../contexts/VisitedPagesContext'
 
 // Handles redirection logic based on query parameters
 const Redirector = () => {
     const { pathname } = useLocation()
+    const navigate = useNavigate()
     const { markPageAsVisited } = useVisitedPages()
 
     useEffect(() => {
         // Extract page name from pathname and increment visit count
         const pageName = pathname.replace('/', '')
+        console.log(`PAGENAME: ${pageName}`)
 
         if (pageName) {
             markPageAsVisited(pageName)
