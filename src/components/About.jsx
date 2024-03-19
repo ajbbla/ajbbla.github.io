1082c0c3f7c1690691ee6d2a20b22a4305bcd754
allenjbb <ajbbla@gmail.com>
2024-03-18 21:55:36 -0700

    Simplify page names

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import AboutLight from '../assets/images/about-light.png'
 import AboutDark from '../assets/images/about-dark.png'
 
 const About = () => {
-    const { visitCount } = usePageVisit('aboutPage')
+    const { visitCount } = usePageVisit('about')
     const [isTypingDone, setIsTypingDone] = useState(false)
     const CURSOR_CLASS_NAME = 'custom-type-animation-cursor'
 
